d4ea6627dfdcbda6ffee28519cc2900a33e914a9
cvlian <cvlian09@gmail.com>
2021-09-06 17:21:06 +0900

    change DB schema/field keys

--- fdl/bank.f
+++ fdl/bank.f
@@ -5,4 +5,5 @@ REC_ID      101     string  -   -
 AMOUNT      102     int     -   -
 MY_BANK     103     string  -   -
 REC_BANK    104     string  -   -
+DATE        105     string  -   -
 MESSAGE     106     string  -   -
